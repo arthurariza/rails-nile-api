0a16d04d0bbe27b51a06b35ea73125d53f1c5ddb
arthurariza <tucaariza@gmail.com>
2022-12-03 17:53:31 -0300

    Book destroy and exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+
+  private
+
+  def not_found
+    head :bad_request
+  end
+
+  def not_destroyed
+    render json: { errors: e.record.errors }, status: :unprocessable_entity
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,3 @@
 class Book < ApplicationRecord
+  validates :author, :title, presence: true, length: { minimum: 3 }
 end
