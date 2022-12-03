5fc1f009ba7c348285069e26b3b6d38715227988
arthurariza <tucaariza@gmail.com>
2022-12-03 19:07:52 -0300

    Book post, delete specs

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,13 +1,49 @@
 require 'rails_helper'
 
 describe 'Books API', type: :request do
-  it 'returns all the books' do
-    FactoryBot.create(:book, title: '1984', author: 'George Orwel')
-    FactoryBot.create(:book, title: 'The Time Machine', author: 'H.G. Wells')
+  describe 'GET /books' do
+    it 'returns all the books' do
+      FactoryBot.create(:book, title: '1984', author: 'George Orwel')
+      FactoryBot.create(:book, title: 'The Time Machine', author: 'H.G. Wells')
 
-    get '/api/v1/books'
+      get '/api/v1/books'
 
-    expect(response).to have_http_status(:success)
-    expect(JSON.parse(response.body)['Books'].size).to eq(2)
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)['Books'].size).to eq(2)
+    end
+  end
+
+  describe 'POST /books' do
+    it 'creates a new book' do
+      book = { title: '1984', author: 'George Orwel' }
+
+      expect do
+        post '/api/v1/books', params: { book: }
+      end.to change { Book.count }.from(0).to(1)
+
+      expect(response).to have_http_status(:created)
+    end
+
+    it 'returns unprocessable_entity when validation fails' do
+      book = { title: '', autor: 'Blank' }
+
+      expect do
+        post '/api/v1/books', params: { book: }
+      end.not_to change { Book.count }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+
+  describe 'DELETE /books/:id' do
+    it 'deletes a book' do
+      book = FactoryBot.create(:book, title: '1984', author: 'George Orwel')
+
+      expect do
+        delete "/api/v1/books/#{book.id}"
+      end.to change { Book.count }.from(1).to(0)
+
+      expect(response).to have_http_status(:no_content)
+    end
   end
 end
